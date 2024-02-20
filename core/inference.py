976734bdf0740a779eea938448b19860d84fb0d3
Chun-Yu Lin <chunyu612016@gmail.com>
2024-02-20 23:10:44 +0800

    refractor: move function "draw_inference_result" from inference.py to utils.py

--- core/inference.py
+++ core/inference.py
@@ -1,5 +1,3 @@
-from PIL import Image
-import cv2
 from ultralytics import YOLO
 
 
@@ -22,33 +20,7 @@ def human_bbox_prediction(image):
     return conf, bbox
     
 
-def draw_inference_result(image, conf, bbox):
-    """
-    Draws inference results on the input image.
-    
-    Parameters:
-    - image: the input image in cv2 format
-    - conf: confidence values for the inference results
-    - bbox: bounding boxes for the inference results
-    
-    Returns:
-    - im_rgb: the image with the inference results drawn in PIL image format
-    """
-    fontface = cv2.FONT_HERSHEY_SIMPLEX
-    for i, box in enumerate(bbox):
-        x1, y1, x2, y2 = [int(coord) for coord in box]
-        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-        label = f'person: {conf[i]:.2f}'
-        t_size = cv2.getTextSize(label, fontface, 0.4, 2)[0]
-        c2 = x1 + t_size[0], y1 - t_size[1] - 12
-        cv2.rectangle(image, (x1, y1), c2, (0, 255, 0), -1)
-
-        cv2.putText(image, label, (x1, y1 - 8), fontface, 0.4, (0, 0, 0), 1, cv2.LINE_AA)
-
-    im_rgb = Image.fromarray(image[..., ::-1])
-
-    return im_rgb
 
 
 
--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,35 @@
 import cv2
 import numpy as np
+from PIL import Image
+
+
+def draw_inference_result(image, conf, bbox):
+    """
+    Draws inference results on the input image.
+    
+    Parameters:
+    - image: the input image in cv2 format
+    - conf: confidence values for the inference results
+    - bbox: bounding boxes for the inference results
+    
+    Returns:
+    - im_rgb: the image with the inference results drawn in PIL image format
+    """
+    fontface = cv2.FONT_HERSHEY_SIMPLEX
+    for i, box in enumerate(bbox):
+        x1, y1, x2, y2 = [int(coord) for coord in box]
+        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+        label = f'person: {conf[i]:.2f}'
+        t_size = cv2.getTextSize(label, fontface, 0.4, 2)[0]
+        c2 = x1 + t_size[0], y1 - t_size[1] - 12
+        cv2.rectangle(image, (x1, y1), c2, (0, 255, 0), -1)
+
+        cv2.putText(image, label, (x1, y1 - 8), fontface, 0.4, (0, 0, 0), 1, cv2.LINE_AA)
+
+    im_rgb = Image.fromarray(image[..., ::-1])
+
+    return im_rgb
 
 
 def draw_alarm_region(image):
@@ -27,5 +57,8 @@ def draw_alarm_region(image):
     beta = 0.5
     gamma = 0
     mask_img = cv2.addWeighted(image, alpha, zero_mask, beta, gamma)
+
+    # 轉回 RGB
+    mask_img_rgb = Image.fromarray(mask_img[..., ::-1])
 
     return mask_img
