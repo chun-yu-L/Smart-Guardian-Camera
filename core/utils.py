d1f56a8ae3758529762fd4d48144292e9f22a884
Chun-Yu Lin <chunyu612016@gmail.com>
2024-02-25 16:14:48 +0800

    refractor: refractor image processing functions into a class

--- core/utils.py
+++ core/utils.py
@@ -3,68 +3,73 @@ import numpy as np
 from PIL import Image
 
 
-def draw_inference_result(image, conf, bbox):
-    """
-    Draws inference results on the input image.
-    
-    Args:
-    - image: the input image in cv2 format
-    - conf: confidence values for the inference results
-    - bbox: bounding boxes for the inference results
-    
-    Returns:
-    - im_rgb: the image with the inference results drawn in BGR format (openCV)
-    """
-    fontface = cv2.FONT_HERSHEY_SIMPLEX
-    for i, box in enumerate(bbox):
-        x1, y1, x2, y2 = [int(coord) for coord in box]
-        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+class YoloImageProcessing:
+    def __init__(self):
+        pass
 
-        label = f'person: {conf[i]:.2f}'
-        t_size = cv2.getTextSize(label, fontface, 0.4, 2)[0]
-        c2 = x1 + t_size[0], y1 - t_size[1] - 12
-        cv2.rectangle(image, (x1, y1), c2, (0, 255, 0), -1)
+    def draw_inference_result(self, image, conf, bbox):
+        """
+        Draws inference results on the input image.
+        
+        Args:
+        - image: the input image in cv2 format
+        - conf: confidence values for the inference results
+        - bbox: bounding boxes for the inference results
+        
+        Returns:
+        - im_rgb: the image with the inference results drawn in BGR format (openCV)
+        """
+        fontface = cv2.FONT_HERSHEY_SIMPLEX
+        for i, box in enumerate(bbox):
+            x1, y1, x2, y2 = [int(coord) for coord in box]
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-        cv2.putText(image, label, (x1, y1 - 8), fontface, 0.4, (0, 0, 0), 1, cv2.LINE_AA)
+            label = f'person: {conf[i]:.2f}'
+            t_size = cv2.getTextSize(label, fontface, 0.4, 2)[0]
+            c2 = x1 + t_size[0], y1 - t_size[1] - 12
+            cv2.rectangle(image, (x1, y1), c2, (0, 255, 0), -1)
 
-    return image
+            cv2.putText(image, label, (x1, y1 - 8), fontface, 0.4, (0, 0, 0), 1, cv2.LINE_AA)
 
-
-def draw_alarm_region(image):
-    """
-    Draws an alarm region on the input image.
-
-    Args:
-    - image: The input image to draw the alarm region on.
-
-    Returns:
-    - The input image with the alarm region drawn on it.
-    """
-    # 5邊形座標點
-    points = np.array([[1460, 0], [1820, 0], [800, 1075], [0, 1075], [0, 900]], np.int32)
-
-    # 畫出 mask
-    zero = np.zeros((image.shape), dtype=np.uint8)
-    zero_mask = cv2.fillPoly(zero, [points], color=(0, 0, 200), lineType=cv2.LINE_AA, shift=0)
-
-    # 把 mask 疊到圖片上
-    alpha = 1
-    beta = 0.5
-    gamma = 0 # 曝光度
-    mask_img = cv2.addWeighted(image, alpha, zero_mask, beta, gamma)
-
-    return mask_img
+        return image
 
 
-def save_image(image, path):
-    """
-    Save an image to the specified path.
+    def draw_alarm_region(self, image):
+        """
+        Draws an alarm region on the input image.
 
-    Args:
-        image: The input image as a numpy array in BGR format.
-        path: The file path where the image will be saved.
-    """
-    img_PIL = Image.fromarray(image[..., ::-1])
-    img_PIL.save(path)
+        Args:
+        - image: The input image to draw the alarm region on.
+
+        Returns:
+        - The input image with the alarm region drawn on it.
+        """
+        # 5邊形座標點
+        points = np.array([[1460, 0], [1820, 0], [800, 1075], [0, 1075], [0, 900]], np.int32)
+
+        # 畫出 mask
+        zero = np.zeros((image.shape), dtype=np.uint8)
+        zero_mask = cv2.fillPoly(zero, [points], color=(0, 0, 200), lineType=cv2.LINE_AA, shift=0)
+
+        # 把 mask 疊到圖片上
+        # alpha, beta 控制透明度；gamma 控制曝光度
+        alpha = 1
+        beta = 0.5
+        gamma = 0
+        mask_img = cv2.addWeighted(image, alpha, zero_mask, beta, gamma)
+
+        return mask_img
+
+
+    def save_image(self, image, path):
+        """
+        Save an image to the specified path.
+
+        Args:
+            image: The input image as a numpy array in BGR format.
+            path: The file path where the image will be saved.
+        """
+        img_PIL = Image.fromarray(image[..., ::-1])
+        img_PIL.save(path)
 
     
