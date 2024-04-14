6c25819c7c868ff813e3c63a80ea6e8eec0db3ce
Chun-Yu Lin <chunyu612016@gmail.com>
2024-04-14 14:13:46 +0800

    feat: Improve distance calculation and visualization for alert distance
    - Implemented a function to calculate the distance of detected objects to the alert zone using perspective transformation
    
    - Added a function to draw the bounding boxes and display the distance information in different colors based on the proximity to the alert zone (inside, safe, near)
    
    - This enhancement improves the visualization of the alert zone and provides more detailed information for monitoring and decision-making

--- core/utils.py
+++ core/utils.py
@@ -38,25 +38,30 @@ def detect_person_in_alert_zone(bbox):
             
     return bbox_inside, bbox_outside
 
-def calculate_distance_to_alert_zone(bbox, perspective_transform):
+def calculate_distance_to_alert_zone(bbox, conf, perspective_transform):
     """
     Calculate the distance of each bounding box to the alert zone based on the provided perspective transform.
     
     Args:
         bbox (numpy.ndarray): An array of bounding boxes representing coordinates of detected objects.
+        conf (numpy.ndarray): An array of confidence values for the inference results
         perspective_transform (PerspectiveTransform): An instance of the PerspectiveTransform class.
     
     Returns:
         dict: A dictionary with bounding boxes sorted into 'inside', 'safe', and 'near' zones, each with its distance to the alert zone.
     """
 
-    alert_region = np.array([[1460, 0], [1820, 0], [800, 1075], [0, 1075], [0, 900]], np.int32)
+    alert_region = np.array([[1460, 0], [1820, 0], [800, 1075], [0, 1075], [0, 900]], np.float32)
     alert_region_transform = perspective_transform.transform_points(alert_region)
 
-    bbox_distance_dict = {'inside': ([], []), 'safe': ([], []), 'near': ([], [])}
+    bbox_distance_dict = {
+        'inside': ([], [], []),
+        'safe': ([], [], []),
+        'near': ([], [], [])
+        }
     
     # 判斷人物是否在警戒區
-    for coord in bbox:
+    for i, coord in enumerate(bbox):
         # 計算人物的座標底部中點
         bottom_center = np.array([(coord[0] + coord[2]) / 2, coord[3]])
         
@@ -64,22 +69,25 @@ def calculate_distance_to_alert_zone(bbox, perspective_transform):
         transform_bottom_center = perspective_transform.transform_points(bottom_center)
         
         # 計算人物到警戒區邊界的距離
-        distance = cv2.pointPolygonTest(alert_region_transform, transform_bottom_center, True)
+        distance = -cv2.pointPolygonTest(alert_region_transform, transform_bottom_center, True)
 
-        if distance > 3:
+        if distance > 5:
             # 人物遠離警戒區
             bbox_distance_dict['safe'][0].append(coord)
             bbox_distance_dict['safe'][1].append(distance)
+            bbox_distance_dict['safe'][2].append(conf[i])
 
-        elif 0 < distance <= 3:
+        elif 0 < distance <= 5:
             # 人物靠近警戒區
             bbox_distance_dict['near'][0].append(coord)
             bbox_distance_dict['near'][1].append(distance)
+            bbox_distance_dict['near'][2].append(conf[i])
 
         else:
             # 人物在警戒區內
             bbox_distance_dict['inside'][0].append(coord)
             bbox_distance_dict['inside'][1].append(distance)
+            bbox_distance_dict['inside'][2].append(conf[i])
 
     return bbox_distance_dict
     
@@ -87,6 +95,7 @@ def calculate_distance_to_alert_zone(bbox, perspective_transform):
 class YoloImageProcessing:
     def __init__(self):
         self.fontface = cv2.FONT_HERSHEY_SIMPLEX
+        self.fontscale = 0.7
         self.alarm_region = np.array([[1460, 0], [1820, 0], [800, 1075], [0, 1075], [0, 900]], np.int32)
 
     def draw_inference_result(self, image, conf, bbox, color = 'blue'):
@@ -115,14 +124,50 @@ class YoloImageProcessing:
             cv2.rectangle(image, (x1, y1), (x2, y2), selected_color, 2)
 
             label = f'person: {conf[i]:.2f}'
-            t_size = cv2.getTextSize(label, self.fontface, 0.5, 2)[0]
+            t_size = cv2.getTextSize(label, self.fontface, self.fontscale, 2)[0]
             c2 = x1 + t_size[0], y1 - t_size[1] - 12
             cv2.rectangle(image, (x1, y1), c2, selected_color, -1)
 
-            cv2.putText(image, label, (x1, y1 - 8), self.fontface, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+            cv2.putText(image, label, (x1, y1 - 8), self.fontface, self.fontscale, (255, 255, 255), 1, cv2.LINE_AA)
 
         return image
+    
+    def draw_alert_distances(self, image, bbox_distance_dict):
+        """
+        Draws alert distances on the input image based on the bbox_distance_dict provided.
 
+        Parameters:
+            image (cv2 format): The image on which to draw the alert distances.
+            bbox_distance_dict (dict): A dictionary containing information about bounding boxes, distances, and confidences.
+
+        Returns:
+            image (cv2 format): The image with the alert distances drawn.
+        """
+        colors = {'inside': (0, 0, 255), 'safe': (255, 0, 0), 'near': (0, 220, 255)}  # 定義不同區域的顏色
+
+        for zone, (bboxes, distances, confidences) in bbox_distance_dict.items():
+            color = colors[zone]  # 獲取區域對應的顏色
+            for box, distance, conf in zip(bboxes, distances, confidences):
+                x1, y1, x2, y2 = [int(coord) for coord in box]  # 獲取邊界框的座標
+                cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)  # 繪製邊界框
+
+                # 寫上confidence
+                conf_label = f'person: {conf:.2f}'
+                t_size_conf = cv2.getTextSize(conf_label, self.fontface, self.fontscale, 2)[0]
+                c2 = x1 + t_size_conf[0], y1 - t_size_conf[1] - 12 # 計算文字標籤框的右上角座標
+                cv2.rectangle(image, (x1, y1), c2, color, -1)
+                cv2.putText(image, conf_label, (x1, y1 - 8), self.fontface, self.fontscale, (255, 255, 255), 2, cv2.LINE_AA)
+
+                # 寫上距離
+                if distance > 0:
+                    distance_label = f'Distance: {distance:.2f}'
+                    t_size_dist = cv2.getTextSize(distance_label, self.fontface, self.fontscale, 2)[0]
+                    c3 = x1 + t_size_dist[0], c2[1] - t_size_dist[1] - 12 # 計算文字標籤框的右上角座標
+                    cv2.rectangle(image, (x1, c2[1]), c3, color, -1)
+                    cv2.putText(image, distance_label, (x1, c2[1] - 8), self.fontface, self.fontscale, (255, 255, 255), 2, cv2.LINE_AA)
+
+        return image
+    
 
     def draw_alarm_region(self, image):
         """
