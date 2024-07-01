3ac0f18b3f5007b7d7d0065023f070ea22797f48
Chun-Yu Lin <chunyu612016@gmail.com>
2024-07-01 22:32:39 +0800

    feat: Human tracking functionality with YOLO model
    - 使用 Yolo v8 提供的 track 功能來獲得 bbox id
    
    - 預設為使用 BoT-SORT，亦可改成 ByteTracker 或其他自訂物件追蹤模型

--- core/inference.py
+++ core/inference.py
@@ -1,5 +1,5 @@
-from ultralytics import YOLO
 import numpy as np
+from ultralytics import YOLO
 
 
 def human_bbox_prediction(image):
@@ -10,11 +10,11 @@ def human_bbox_prediction(image):
         image: The input image to be processed.
 
     Returns:
-        conf: Numpy array of confidence values for the inference results.
         bbox: Numpy array of the predicted bounding box coordinates in the format (xmin, ymin, xmax, ymax).
+        conf: Numpy array of confidence values for the inference results.
     """
-    # 預測 bbox 與 conf
-    model = YOLO('yolov8n.pt')
+    # 預測 bbox
+    model = YOLO("yolov8n.pt")
     results = model(image, conf=0.4, classes=[0])
 
     bbox = results[0].boxes.xyxy.numpy()
@@ -31,9 +31,38 @@ def human_bbox_prediction(image):
     bbox[:, 3] = np.clip(bbox[:, 3], 0, height)
 
     return bbox, conf
-    
 
 
+def human_tracker_yolo(image):
+    """
+    Predicts bounding boxes and IDs for humans in the given image using YOLO model.
 
+    Args:
+        image: The input image to be processed.
 
+    Returns:
+        bbox: Numpy array of the predicted bounding box coordinates in the format (xmin, ymin, xmax, ymax).
+        conf: Numpy array of confidence values for the inference results.
+        id: A numpy array of the IDs associated with the predicted bounding boxes.
 
+    This function uses the YOLO model to predict bounding boxes and IDs for humans in the given image. It takes an RGB image as input and returns the predicted bounding boxes, confidence values, and IDs.
+    """
+    # 預測 bbox 與 ID
+    model = YOLO("yolov8n.pt")
+    results = model.track(image, conf=0.4, classes=[0], persist=True)
+
+    bbox = results[0].boxes.xyxy.numpy()
+    conf = results[0].boxes.conf.numpy()
+    id = results[0].boxes.id.numpy()
+
+    # 超出圖片的 bbox 縮回來
+    # 獲取圖片尺寸
+    height, width, _ = image.shape
+
+    # 修正 bbox 座標
+    bbox[:, 0] = np.clip(bbox[:, 0], 0, width)
+    bbox[:, 1] = np.clip(bbox[:, 1], 0, height)
+    bbox[:, 2] = np.clip(bbox[:, 2], 0, width)
+    bbox[:, 3] = np.clip(bbox[:, 3], 0, height)
+
+    return bbox, conf, id
