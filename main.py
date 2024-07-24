1d85b149292098bcc7b9b982edc8d74a3f9c0705
Chun-Yu Lin <chunyu612016@gmail.com>
2024-07-24 15:28:38 +0800

    feat: Add human tracking functionality API endpoint
    - Introduce new endpoint for human tracking in /tracking/
    
    - The commit also refactored endpoint structure

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-
 import io
-import numpy as np
-from PIL import Image
+
 import cv2
+import numpy as np
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
-from core.inference import human_bbox_prediction
-from core.utils import YoloImageProcessing, PerspectiveTransform, AlertDetector
+from PIL import Image
+
+from core.inference import human_bbox_prediction, human_tracker_yolo
+from core.utils import AlertDetector, PerspectiveTransform, YoloImageProcessing
 
 app = FastAPI()
+
 
 @app.post("/detect/")
 async def detect(file: UploadFile = File(...)):
@@ -30,7 +32,7 @@ async def detect(file: UploadFile = File(...)):
     return StreamingResponse(buffer, media_type="image/jpeg")
 
 
-@app.post("/inside_outside_alert/")
+@app.post("/detect/inside_outside_alert/")
 async def is_inside_alert_zone(file: UploadFile = File(...)):
     # 讀取上傳的圖片
     pil_img = Image.open(io.BytesIO(await file.read()))
@@ -54,7 +56,8 @@ async def is_inside_alert_zone(file: UploadFile = File(...)):
     buffer.seek(0)
     return StreamingResponse(buffer, media_type="image/jpeg")
 
-@app.post("/distance_to_alert_zone/")
+
+@app.post("/detect/distance_to_alert_zone/")
 async def distance_to_alert_zone(file: UploadFile = File(...)):
     # 讀取上傳的圖片
     pil_img = Image.open(io.BytesIO(await file.read()))
@@ -74,6 +77,35 @@ async def distance_to_alert_zone(file: UploadFile = File(...)):
     # 繪製結果
     YoloPlot = YoloImageProcessing()
     result = YoloPlot.draw_alert_distances(img, bbox_dict)
+    result = YoloPlot.draw_alert_zone(result)
+
+    # 回傳圖片
+    buffer = io.BytesIO()
+    Image.fromarray(cv2.cvtColor(result, cv2.COLOR_BGR2RGB)).save(buffer, format="jpeg")
+    buffer.seek(0)
+    return StreamingResponse(buffer, media_type="image/jpeg")
+
+
+@app.post("/tracking/")
+async def distance_with_ids(file: UploadFile = File(...)):
+    # 讀取上傳的圖片
+    pil_img = Image.open(io.BytesIO(await file.read()))
+    img = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
+
+    # 人物追蹤
+    bbox, conf, ids = human_tracker_yolo(img)
+
+    # 視角轉換
+    src_point = np.array([[0, 900], [1460, 0], [1820, 0], [800, 1075]], np.float32)
+    PT = PerspectiveTransform(src_point, 50, 20)
+
+    # 判斷與警戒區的距離
+    detector = AlertDetector()
+    bbox_dict = detector.distance_with_ids(bbox, conf, ids, PT)
+
+    # 繪製結果
+    YoloPlot = YoloImageProcessing()
+    result = YoloPlot.draw_distance_with_id(img, bbox_dict)
     result = YoloPlot.draw_alert_zone(result)
 
     # 回傳圖片
